ec50783d04e183bc023300c340197de796643cc1
Greg Munro <gregmunro90@gmail.com>
2022-01-22 15:09:33 +0100

    update delete icon

--- myapp/src/components/Task.js
+++ myapp/src/components/Task.js
@@ -1,12 +1,21 @@
-import { FaTimes } from 'react-icons/fa'
+import { BsTrash } from "react-icons/bs";
 
 const Task = ({ task, onDelete, onToggle }) => {
   return (
-  <div className={`task ${task.reminder ?  'reminder' : ''}`} onDoubleClick={()=>onToggle(task.id)}>
-<h3>{task.text} <FaTimes style={{color:'red', curso: 'pointer'}} onClick={() => onDelete(task.id)} /></h3>
-<p>{task.day}</p>
-  </div>
-  )
+    <div
+      className={`task ${task.reminder ? "reminder" : ""}`}
+      onDoubleClick={() => onToggle(task.id)}
+    >
+      <h3>
+        {task.text}{" "}
+        <BsTrash
+          style={{ color: "black", curso: "pointer" }}
+          onClick={() => onDelete(task.id)}
+        />
+      </h3>
+      <p>{task.day}</p>
+    </div>
+  );
 };
-  
+
 export default Task;
